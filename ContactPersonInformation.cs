0d1dbbcb31a28c99f95b14ab685c9543f27ef826
Abhishek Narwariya <abhisheknarwariya813@gmail.com>
2022-05-12 14:35:46 +0530

    adding UC1

---
+++ ContactPersonInformation.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace AddressBook
+{
+    class ContactPersonInformation
+    {
+        List<ContactDetails> contactDetailsList;
+        private readonly NLog nLog = new NLog();
+
+        public ContactPersonInformation()
+        {
+            contactDetailsList = new List<ContactDetails>();
+        }
+        public void AddingContactDetails(string firstName, string lastName, string address, string city, string state, int zip, double phoneNo, string eMail)
+        {
+            if (phoneNo <= 200000)
+            {
+               
+                nLog.LogWarn("Entered Wrong Phone no. : AdditionContactDetails()");
+            }
+            ContactDetails contactDetails = new ContactDetails(firstName, lastName, address, city, state, zip, phoneNo, eMail);
+            if (contactDetailsList.Contains(contactDetails))
+            {
+                nLog.LogError("Contact details have already been entered");
+                Console.WriteLine("Contact details have already been entered\n please add new contact details");
+
+            }
+            else
+            {
+                contactDetailsList.Add(contactDetails);
+                nLog.LogInfo("Contact Details Addition Successful: AddingContactDetails()");
+            }
+
+
+        }
+        public void DisplayContactDetails()
+        {
+            foreach (ContactDetails contactPerson in contactDetailsList)
+            {
+                Console.WriteLine($"First Name : {contactPerson.firstName} || Last Name: {contactPerson.lastName} || Address: {contactPerson.address} || City: {contactPerson.city} || State: {contactPerson.state}|| zip: {contactPerson.zip} || Phone No: {contactPerson.phoneNo} || eMail: {contactPerson.eMail}");
+
+            }
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AddressBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book Management System");
+            ContactPersonInformation contactPersonInformation = new ContactPersonInformation();
+            contactPersonInformation.AddingContactDetails("Abhishek", "Narwariya", "a", "b", "c", 1, 2, "abhisheknarwariya813@gmail.com");
+            contactPersonInformation.AddingContactDetails("anup", "lodhi", "a", "b", "d", 3, 5, "anuplodhi420@gmail.com");
+            contactPersonInformation.DisplayContactDetails();
+        }
+    }
+}
