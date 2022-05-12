cbaa12d722efd5573044dedb60f54695fa348210
Abhishek Narwariya <abhisheknarwariya813@gmail.com>
2022-05-12 14:56:21 +0530

    adding UC3

--- Program.cs
+++ Program.cs
@@ -9,8 +9,16 @@ namespace AddressBook
             Console.WriteLine("Welcome to Address Book Management System");
             ContactPersonInformation contactPersonInformation = new ContactPersonInformation();
             contactPersonInformation.AddingContactDetails();
-           
+            
             contactPersonInformation.DisplayContactDetails();
+            Console.WriteLine("Do you want to edit any details, enter y to edit.");
+            string input = Console.ReadLine();
+            if (input.ToLower() == "y")
+            {
+                contactPersonInformation.EditingContactDetails();
+                contactPersonInformation.DisplayContactDetails();
+
+            }
         }
     }
 }
